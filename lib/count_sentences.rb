c77813a4b856f4e942baf56e7c846c47f68b1389
Andrew Klein <andrew.klein.j@gmail.com>
2016-07-27 17:58:59 -0400

    complete assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "."
   end
 
   def question?
-
+    self[-1] == "?"
   end
 
   def exclamation?
-
+    self[-1] == "!"
   end
 
   def count_sentences
-
+    new_string = self.tr("!",".")
+    new_string = new_string.tr("?",".")
+    split_sentences = new_string.split(".")
+    split_sentences.delete("")
+    split_sentences.length
   end
 end
